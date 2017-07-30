686ad66541c1baf3094a20c612a95cea1512d96d
Naruth Kongurai <naruth1212@gmail.com>
2017-07-31 03:57:50 +0700

    Fixed JS error

--- js/theme.js
+++ js/theme.js
@@ -1,10 +1,13 @@
 "use strict";
 
-(function() {
-    window.onload = function() {
-        alert('ee');
-        document.getElementById("moreProjectsBtn").onclick = function() {
-            document.getElementById("moreProjectsBtn").style.display = 'none';
-        }
-    }
-})
+document.getElementById("lessProjectsBtn").classList.add('hidden');
+
+document.getElementById("moreProjectsBtn").onclick = function() {
+    document.getElementById("moreProjectsBtn").classList.add('hidden');
+    document.getElementById("lessProjectsBtn").classList.remove('hidden');
+};
+
+document.getElementById("lessProjectsBtn").onclick = function() {
+    document.getElementById("moreProjectsBtn").classList.remove('hidden');
+    document.getElementById("lessProjectsBtn").classList.add('hidden');
+};

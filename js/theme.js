22f23867f300bc95574673da2fff0ce08a327593
Naruth Kongurai <naruth1212@gmail.com>
2017-08-01 16:38:12 +0700

    Fixed projects section, removed clutter, fixed JS errors

--- js/theme.js
+++ js/theme.js
@@ -1,13 +1,5 @@
 "use strict";
 
-document.getElementById("lessProjectsBtn").classList.add('hidden');
-
 document.getElementById("moreProjectsBtn").onclick = function() {
     document.getElementById("moreProjectsBtn").classList.add('hidden');
-    document.getElementById("lessProjectsBtn").classList.remove('hidden');
-};
-
-document.getElementById("lessProjectsBtn").onclick = function() {
-    document.getElementById("moreProjectsBtn").classList.remove('hidden');
-    document.getElementById("lessProjectsBtn").classList.add('hidden');
 };

f80ebd960bef1ade95ba3ebee49f846d9706078f
Naruth Kongurai <naruth1212@gmail.com>
2017-10-17 21:11:40 -0700

    Project section redesigned. Initial markup completed

--- js/theme.js
+++ js/theme.js
@@ -2,10 +2,6 @@
 
 $(document).ready(function () {
 
-    $('#projects-button-click').click(function() {
-        $('#projects-button-wrapper').hide();
-    });
-
     $(window).scroll(function () {
         if ($(this).scrollTop() > 100) {
             $('.scroll-to-top').fadeIn();
@@ -20,5 +16,18 @@ $(document).ready(function () {
         }, 600);
         return false;
     });
+
+
+    var selectedClass = "";
+    $(".fil-cat").click(function() { 
+        selectedClass = $(this).attr("data-rel"); 
+        $("#portfolio").fadeTo(100, 0.1);
+        $("#portfolio div").not("."+selectedClass).fadeOut().removeClass('scale-anm');
+        setTimeout(function() {
+            $("."+selectedClass).fadeIn().addClass('scale-anm');
+            $("#portfolio").fadeTo(300, 1);
+        }, 300); 
+    });
+
 
 });
